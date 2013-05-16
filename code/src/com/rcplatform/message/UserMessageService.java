e9c5f4688432a22bfc8bfc3bf743751fb4b76b8d
xiaobaixurc <xbx@rcplatformhk.com>
2013-05-16 20:57:57 +0800

    gcm 基本调通。

--- code/src/com/rcplatform/message/UserMessageService.java
+++ code/src/com/rcplatform/message/UserMessageService.java
@@ -1,15 +1,28 @@
 package com.rcplatform.message;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Timer;
+import java.util.TimerTask;
 
+import org.apache.http.protocol.HTTP;
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ChatManagerListener;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import com.google.android.gcm.MetaHelper;
+import com.rcplatform.phototalk.MenueApplication;
 import com.rcplatform.tigase.TigaseNode;
 import com.rcplatform.tigase.TigaseNodeUtil;
 import com.rcplatform.tigase.XmppTool;
@@ -23,6 +36,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Vibrator;
+import android.util.Log;
 
 public class UserMessageService extends Service {
 
@@ -49,18 +63,20 @@ public class UserMessageService extends Service {
 	private static final String MESSAGE_TYPE_RECEIPT = "r";
 
 	private static final String MESSAGE_SPLIT = ":";
-	
+
 	public static final String MESSAGE_ACTION_KEY = "action";
-	
+
 	public static final String MESSAGE_ACTION_MSG = "1";
-	
+
 	public static final String MESSAGE_ACTION_FRIEND = "2";
+
+	public static final String MESSAGE_RCID_KEY = "rcid";
 	
-	public static final String MESSAGE_RCID_KEY="rcid";
+	private static final String GCM_URL = "http://192.168.0.56:8080/phototalk/user/pushOfflineMsg.do";
 
 	private Context ctx;
-	
-	private HashMap<String,Timer> gcmTimers;
+
+	private HashMap<String, Timer> gcmTimers;
 
 	private boolean hasRegisteSendReceiver = false;
 
@@ -76,12 +92,11 @@ public class UserMessageService extends Service {
 					String str = message.getBody();
 					int typeEnd = str.indexOf(MESSAGE_SPLIT);
 					String msgType = str.substring(0, typeEnd);
-					str = str.substring(typeEnd+1);
+					str = str.substring(typeEnd + 1);
 					int actionEnd = str.indexOf(MESSAGE_SPLIT);
-					String action = str.substring(0,actionEnd);
-					String msgContent = str.substring(actionEnd+1);
-
+					String action = str.substring(0, actionEnd);
 					if (msgType.equals(MESSAGE_TYPE_MESSAGE)) {
+						String msgContent = str.substring(actionEnd + 1);
 						Intent intent = new Intent();
 						intent.setAction(MESSAGE_RECIVE_BROADCAST);
 						// 要发送的内容
@@ -91,17 +106,21 @@ public class UserMessageService extends Service {
 						ctx.sendBroadcast(intent);
 
 						try {
-							chat.sendMessage(MESSAGE_TYPE_RECEIPT + MESSAGE_SPLIT + action);
+							chat.sendMessage(MESSAGE_TYPE_RECEIPT + MESSAGE_SPLIT + action+MESSAGE_SPLIT);
 						}
 						catch (XMPPException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-					} else if (msgType.equals(MESSAGE_SPLIT)) {
+					} else if (msgType.equals(MESSAGE_TYPE_RECEIPT)) {
 						// TODO 取消gcm 发送
-						String cancelKey = message.getFrom()+action;
+						
+						String formUser= message.getFrom();
+						int end = formUser.indexOf("/");
+						formUser = formUser.substring(0, end);
+						String cancelKey =  formUser + action;
 						Timer cancelTimer = gcmTimers.get(cancelKey);
-						if(null != cancelTimer){
+						if (null != cancelTimer) {
 							cancelTimer.cancel();
 							gcmTimers.remove(cancelKey);
 						}
@@ -119,10 +138,18 @@ public class UserMessageService extends Service {
 			Bundle extras = intent.getExtras();
 			String toUser = extras.getString(MESSAGE_TO_USER);
 			String msg = extras.getString(MESSAGE_CONTENT_KEY);
-			
-			String action = extras.getString(MESSAGE_ACTION_KEY);
 
-			XmppTool.sendMessage(toUser, MESSAGE_TYPE_MESSAGE +MESSAGE_SPLIT+ action+ MESSAGE_SPLIT+msg);
+			String action = extras.getString(MESSAGE_ACTION_KEY);
+			String toRcId = extras.getString(MESSAGE_RCID_KEY);
+
+			XmppTool.sendMessage(toUser, MESSAGE_TYPE_MESSAGE + MESSAGE_SPLIT + action + MESSAGE_SPLIT + msg);
+			
+			String timerKey = XmppTool.getFullUser(toUser)+action;
+			
+			Timer timer = new Timer();
+			GcmTask gcmTask = new GcmTask(context,action,toRcId);
+			timer.schedule(gcmTask, 30000);
+			gcmTimers.put(timerKey, timer);
 
 		}
 	};
@@ -137,7 +164,7 @@ public class UserMessageService extends Service {
 	public void onStart(Intent intent, int startId) {
 		super.onStart(intent, startId);
 		createXmppConnection(intent);
-		gcmTimers = new HashMap<String , Timer>() ;
+		gcmTimers = new HashMap<String, Timer>();
 	}
 
 	private void createXmppConnection(final Intent intent) {
@@ -163,12 +190,12 @@ public class UserMessageService extends Service {
 		public void handleMessage(android.os.Message msg) {
 			switch (msg.what) {
 
-			case MSG_WHAT_XMPP_CONNECT_SUCCESS:
-				IntentFilter intentFilter = new IntentFilter();
-				intentFilter.addAction(MESSAGE_SEND_BROADCAST);
-				registerReceiver(sendBroadcastReceiver, intentFilter);
-				hasRegisteSendReceiver = true;
-				break;
+				case MSG_WHAT_XMPP_CONNECT_SUCCESS:
+					IntentFilter intentFilter = new IntentFilter();
+					intentFilter.addAction(MESSAGE_SEND_BROADCAST);
+					registerReceiver(sendBroadcastReceiver, intentFilter);
+					hasRegisteSendReceiver = true;
+					break;
 			}
 		};
 	};
@@ -185,5 +212,107 @@ public class UserMessageService extends Service {
 		XmppTool.closeConnection();
 		if (hasRegisteSendReceiver)
 			this.unregisterReceiver(sendBroadcastReceiver);
+	}
+
+	class GcmTask extends TimerTask {
+
+		private Context ctx;
+
+		private String action;
+
+		private String toRcId;
+
+		GcmTask(Context ctx, String action, String toRcId) {
+			this.ctx = ctx;
+			this.action = action;
+			this.toRcId =toRcId;
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			Log.d("GcmTask", "push to service");
+			
+			JSONObject json = new JSONObject();
+			try {
+				json.put("appId", "1");
+				json.put("type", action);
+				//TODO 设置真是token
+				MenueApplication app = (MenueApplication)ctx.getApplicationContext();
+				
+				json.put("token", app.getCurrentUser().getToken());
+				json.put("fRcId", toRcId);
+				json.put("deviceId", MetaHelper.getMACAddress(ctx));
+				json.put("rcId", app.getCurrentUser().getRcId());
+				json.put("language", "");
+				/*
+				json.put("packageName", MetaHelper.getAppName(context));
+				json.put("status", STATUS_CREATE_USERINFO);
+				json.put("deviceID", MetaHelper.getImsi(context));
+				json.put("clientMac", MetaHelper.getMACAddress(context));
+				json.put("osVersion", MetaHelper.getOsVersion(context));
+				json.put("language", MetaHelper.getLanguage(context));
+				json.put("timeZone", MetaHelper.getTimeZone(context));
+				json.put("timeZoneID", MetaHelper.getTimeZoneId(context));
+				*/
+			}
+			catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			OutputStream output = null;
+			InputStream is = null;
+			HttpURLConnection conn = null;
+			String content = null;
+			try {
+				conn = (HttpURLConnection) new URL(GCM_URL).openConnection();
+				conn.setConnectTimeout(5000);
+				conn.setReadTimeout(5000);
+				conn.setDoOutput(true);
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/json");
+				byte[] bodyBytes = json.toString().getBytes(HTTP.UTF_8);
+				output = conn.getOutputStream();
+				output.write(bodyBytes);
+				is = conn.getInputStream();
+				InputStreamReader reader = new InputStreamReader(is, "UTF-8");
+				StringBuilder builder = new StringBuilder();
+				char[] readChars = new char[1024];
+				String temp = null;
+				int result = -1;
+				while ((result = reader.read(readChars, 0, 1024)) != -1) {
+					temp = new String(readChars, 0, result);
+					builder.append(temp);
+				}
+				reader.close();
+				content = builder.toString();
+			}
+			catch (ConnectException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					output.close();
+				}
+				catch (Exception e) {
+				}
+				try {
+					is.close();
+				}
+				catch (Exception e) {
+				}
+				if (conn != null)
+					conn.disconnect();
+
+			}
+
+
+		}
 	}
 }
