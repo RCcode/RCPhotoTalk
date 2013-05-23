a6ed5bda42a768d10a5399a5e04320e7c41a1f7f
zhaonan <zhaon@rcplatformhk.com>
2013-05-23 16:04:57 +0800

    第三方认证不再需要点击按钮，切换到页面就自动开始认证

--- code/src/com/rcplatform/phototalk/api/PhotoTalkApiUrl.java
+++ code/src/com/rcplatform/phototalk/api/PhotoTalkApiUrl.java
@@ -2,8 +2,8 @@ package com.rcplatform.phototalk.api;
 
 public class PhotoTalkApiUrl {
 
-	 private static final String BASE_URL = "http://192.168.0.86:8083/";
-//	private static final String BASE_URL = "http://192.168.0.56:8080/";
+//	 private static final String BASE_URL = "http://192.168.0.86:8083/";
+	private static final String BASE_URL = "http://192.168.0.56:8080/";
 	// private static final String BASE_URL = "http://103.247.169.150:8083/";
 	// private static final String BASE_URL = "http://192.168.0.118/";
 
