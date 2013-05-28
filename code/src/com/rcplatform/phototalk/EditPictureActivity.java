fa61df11025e09c2960d2c69804d0db48b95cc8a
zhaonan <zhaon@rcplatformhk.com>
2013-05-28 18:48:00 +0800

    修复BUG。图片编辑页面进入时崩溃

--- code/src/com/rcplatform/phototalk/EditPictureActivity.java
+++ code/src/com/rcplatform/phototalk/EditPictureActivity.java
@@ -199,8 +199,8 @@ public class EditPictureActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.edit_picture_view2);
