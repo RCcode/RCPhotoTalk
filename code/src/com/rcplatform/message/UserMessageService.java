54d46591c4c5248ed25b480b3be9db9bc1583184
xiaobaixurc <xbx@rcplatformhk.com>
2013-05-31 16:55:48 +0800

    merge new tigase。

--- code/src/com/rcplatform/message/UserMessageService.java
+++ code/src/com/rcplatform/message/UserMessageService.java
@@ -279,6 +279,16 @@ public class UserMessageService extends Service {
 				json.put("rcId", app.getCurrentUser().getRcId());
 				json.put("language", "");
 				json.put("extra", extra);
+				/*
+				 * json.put("packageName", MetaHelper.getAppName(context));
+				 * json.put("status", STATUS_CREATE_USERINFO);
+				 * json.put("deviceID", MetaHelper.getImsi(context));
+				 * json.put("clientMac", MetaHelper.getMACAddress(context));
+				 * json.put("osVersion", MetaHelper.getOsVersion(context));
+				 * json.put("language", MetaHelper.getLanguage(context));
+				 * json.put("timeZone", MetaHelper.getTimeZone(context));
+				 * json.put("timeZoneID", MetaHelper.getTimeZoneId(context));
+				 */
 			}
 			catch (JSONException e1) {
 				// TODO Auto-generated catch block
