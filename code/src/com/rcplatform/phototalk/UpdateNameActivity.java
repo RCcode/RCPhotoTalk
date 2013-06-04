becc3ea7dca87ede72579d90cbe3936d766998ee
xiaobaixurc <xbx@rcplatformhk.com>
2013-06-04 21:49:38 +0800

    日文完成。

--- code/src/com/rcplatform/phototalk/UpdateNameActivity.java
+++ code/src/com/rcplatform/phototalk/UpdateNameActivity.java
@@ -8,9 +8,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -21,25 +24,33 @@ import com.rcplatform.phototalk.activity.BaseActivity;
 import com.rcplatform.phototalk.utils.Constants;
 import com.rcplatform.phototalk.utils.DialogUtil;
 import com.rcplatform.phototalk.utils.RCPlatformTextUtil;
-//github.com/RCcode/RCPhotoTalk.git
+
+// github.com/RCcode/RCPhotoTalk.git
 
 public class UpdateNameActivity extends BaseActivity implements View.OnClickListener {
 
 	protected static final String TAG = "ForgetPasswordActivity";
+
 	public static final String REQUEST_PARAM_KEY_TEXT = "text";
 
 	private Button mConfirmBtn;
+
 	private ProgressBar mProgressbar;
+
 	private EditText mEditTextView;
+
 	private String currentText;
 
 	private static final int REQUEST_TYPE_SIGNTURE = 1;
+
 	private static final int REQUEST_TYPE_NICK = 0;
 
 	private int requestType = -1;
 
 	private View mBack;
+
 	private TextView mTitleTextView;
+
 	private TextView mUpdateLabelHint;
 
 	@Override
@@ -52,7 +63,30 @@ public class UpdateNameActivity extends BaseActivity implements View.OnClickList
 		mTitleTextView = (TextView) findViewById(R.id.titleContent);
 		mTitleTextView.setVisibility(View.VISIBLE);
 		mUpdateLabelHint = (TextView) findViewById(R.id.update_hint);
+		mUpdateLabelHint.setVisibility(View.GONE);
 		mEditTextView = (EditText) findViewById(R.id.settings_update_edit);
+
+		mEditTextView.setHintTextColor(getResources().getColor(R.color.register_input_hint));
+
+		mEditTextView.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				mEditTextView.setHintTextColor(getResources().getColor(R.color.register_input_hint));
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+			}
+		});
 
 		Intent intent = getIntent();
 		if (intent.getAction().equals("setting_update_name")) {
@@ -60,6 +94,7 @@ public class UpdateNameActivity extends BaseActivity implements View.OnClickList
 			mTitleTextView.setText(getResources().getString(R.string.settings_update_name_title));
 			mUpdateLabelHint.setText(getResources().getString(R.string.settings_update_name_limited_hint));
 			mEditTextView.setFilters(new InputFilter[] { new InputFilter.LengthFilter(RCPlatformTextUtil.NICK_MAX_LENGTH) });
+			mEditTextView.setHint(R.string.login_nick_hint);
 		} else if (intent.getAction().equals("setting_update_signature")) {
 			requestType = REQUEST_TYPE_SIGNTURE;
 			mTitleTextView.setText(getResources().getString(R.string.settings_update_signature_title));
@@ -76,41 +111,48 @@ public class UpdateNameActivity extends BaseActivity implements View.OnClickList
 	private void showDialog(final Context context, String msg) {
 		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
 		dialogBuilder.setMessage(msg).setCancelable(false)
-				.setPositiveButton(context.getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+		        .setPositiveButton(context.getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
 
-					public void onClick(DialogInterface dialog, int which) {
+			        public void onClick(DialogInterface dialog, int which) {
 
-						Intent loginIntent = new Intent(context, LoginActivity.class);
-						loginIntent.putExtra(Constants.KEY_LOGIN_PAGE, true);
-						startActivity(loginIntent);
-						finish();
-					}
-				});
+				        Intent loginIntent = new Intent(context, LoginActivity.class);
+				        loginIntent.putExtra(Constants.KEY_LOGIN_PAGE, true);
+				        startActivity(loginIntent);
+				        finish();
+			        }
+		        });
 		dialogBuilder.create().show();
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.back:
-			finish();
-			break;
-		case R.id.settings_update_confirm_button:
-
-			String label = mEditTextView.getText().toString();
-			// 如何未修改直接退出。
-			if (currentText.equals(label)) {
+			case R.id.back:
 				finish();
-			} else {
-				// 验证输入字符无效
-				if (isNewTextRight(label)) {
-					Intent data = new Intent();
-					data.putExtra("result", label);
-					setResult(Activity.RESULT_OK, data);
+				break;
+			case R.id.settings_update_confirm_button:
+
+				String label = mEditTextView.getText().toString();
+				// 如何未修改直接退出。
+				if (currentText.equals(label)) {
 					finish();
+				} else {
+					// 验证输入字符无效
+					if (label.equals("")) {
+						mEditTextView.setHintTextColor(getResources().getColor(R.color.register_input_hint_error));
+						mEditTextView.requestFocus();
+						InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+						imm.showSoftInput(mEditTextView, 0);
+						return;
+					}
+					if (isNewTextRight(label)) {
+						Intent data = new Intent();
+						data.putExtra("result", label);
+						setResult(Activity.RESULT_OK, data);
+						finish();
+					}
 				}
-			}
-			break;
+				break;
 
 		}
 	}
@@ -119,14 +161,14 @@ public class UpdateNameActivity extends BaseActivity implements View.OnClickList
 		// TODO Auto-generated method stub
 		boolean isRight = false;
 		switch (requestType) {
-		case REQUEST_TYPE_NICK:
-			isRight = checkNickName(text);
-			break;
-		case REQUEST_TYPE_SIGNTURE:
-			isRight = RCPlatformTextUtil.isSigntureMatches(text);
-			if (!isRight)
-				DialogUtil.showToast(getApplicationContext(), R.string.text_too_long, Toast.LENGTH_SHORT);
-			break;
+			case REQUEST_TYPE_NICK:
+				isRight = checkNickName(text);
+				break;
+			case REQUEST_TYPE_SIGNTURE:
+				isRight = RCPlatformTextUtil.isSigntureMatches(text);
+				if (!isRight)
+					DialogUtil.showToast(getApplicationContext(), R.string.text_too_long, Toast.LENGTH_SHORT);
+				break;
 		}
 		return isRight;
 	}
@@ -147,8 +189,8 @@ public class UpdateNameActivity extends BaseActivity implements View.OnClickList
 		final int PWD_MIN_SIZE = 1;
 		final int PWD_MAX_SIZE = 20;
 		if (PWD_MIN_SIZE > label.length() || PWD_MAX_SIZE < label.length()) {
-			DialogUtil.createMsgDialog(this, getResources().getString(R.string.setting_update_name_invalid), getResources().getString(android.R.string.ok))
-					.show();
+			DialogUtil.createMsgDialog(this, getResources().getString(R.string.setting_update_name_invalid),
+			                           getResources().getString(android.R.string.ok)).show();
 			return false;
 		}
 		return true;
@@ -160,11 +202,11 @@ public class UpdateNameActivity extends BaseActivity implements View.OnClickList
 	private boolean checkEmail(String emailId) {
 		// 邮箱正则表达式
 		Pattern emailPattern = Pattern
-				.compile("^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$");
+		        .compile("^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$");
 		final int SMLL_SIZE = 0;
 		if (SMLL_SIZE == emailId.length() || !emailPattern.matcher(emailId).matches()) {
-			DialogUtil.createMsgDialog(this, getResources().getString(R.string.register_noinput_address_info), getResources().getString(android.R.string.ok))
-					.show();
+			DialogUtil.createMsgDialog(this, getResources().getString(R.string.register_noinput_address_info),
+			                           getResources().getString(android.R.string.ok)).show();
 			return false;
 		} else {
 			return true;
