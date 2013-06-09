b66ae4e6676a34f417daf3ffa09a78989f293677
zhaonan <zhaon@rcplatformhk.com>
2013-06-09 14:35:58 +0800

    重置密码成功的toast时间改长

--- code/src/com/rcplatform/phototalk/ForgetPasswordActivity.java
+++ code/src/com/rcplatform/phototalk/ForgetPasswordActivity.java
@@ -91,7 +91,7 @@ public class ForgetPasswordActivity extends BaseActivity implements View.OnClick
 			public void onSuccess(int statusCode, String content) {
 				dismissLoadingDialog();
 				String resetMsg = String.format(getResources().getString(R.string.forget_password_reset_text), mEmail);
-				DialogUtil.showToast(getApplicationContext(), resetMsg, Toast.LENGTH_SHORT);
+				DialogUtil.showToast(getApplicationContext(), resetMsg, Toast.LENGTH_LONG);
 				finish();
 			}
 
