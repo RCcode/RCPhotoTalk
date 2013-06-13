87a4541aac89e4233cf7f225e39fee1d9503fa5a
xiaobaixurc <xbx@rcplatformhk.com>
2013-06-13 16:39:30 +0800

    老王 url。

--- code/src/com/rcplatform/phototalk/api/PhotoTalkApiUrl.java
+++ code/src/com/rcplatform/phototalk/api/PhotoTalkApiUrl.java
@@ -2,7 +2,7 @@ package com.rcplatform.phototalk.api;
 
 public class PhotoTalkApiUrl {
 
-	private static final String BASE_URL = "http://192.168.0.86:8083/";
+	private static final String BASE_URL = "http://192.168.0.13:8080/";
 	// private static final String BASE_URL = "http://192.168.0.111/";
 	// private static final String BASE_URL = "http://pt.rcplatformhk.net/";
 	// private static final String BASE_URL = "http://103.247.169.150:8083/";
@@ -92,7 +92,7 @@ public class PhotoTalkApiUrl {
 
 	// -----------------------------------------------------------------------------------------------------------------------------------
 
-	private static final String RCBOSS_BASE_URL = "http://192.168.0.86:8083/";
+	private static final String RCBOSS_BASE_URL = "http://192.168.0.13:8080/";
 
 	private static final String BASE_URL_FOR_RCBOSS = RCBOSS_BASE_URL + "rcboss/";
 
