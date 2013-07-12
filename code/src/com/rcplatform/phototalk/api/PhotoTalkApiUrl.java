1c10df7e1b65ddaed8a51718a6e17d0d568a9ff9
zhaonan <zhaon@rcplatformhk.com>
2013-07-12 17:49:08 +0800

    URL改回测试环境

--- code/src/com/rcplatform/phototalk/api/PhotoTalkApiUrl.java
+++ code/src/com/rcplatform/phototalk/api/PhotoTalkApiUrl.java
@@ -1,8 +1,8 @@
 package com.rcplatform.phototalk.api;
 
 public class PhotoTalkApiUrl {
-	private static final String BASE_URL = "http://pt.rcplatformhk.net/";
-//	private static final String BASE_URL = "http://192.168.0.86:8083/";
+//	private static final String BASE_URL = "http://pt.rcplatformhk.net/";
+	private static final String BASE_URL = "http://192.168.0.86:8083/";
 //	 private static final String BASE_URL = "http://192.168.0.114/";
 
 	private static final String BASE_URL_FOR_PHOTOTALK = BASE_URL + "phototalk/";
@@ -89,8 +89,8 @@ public class PhotoTalkApiUrl {
 
 	// -----------------------------------------------------------------------------------------------------------------------------------
 
-	private static final String RCBOSS_BASE_URL = "http://rc.rcplatformhk.net/";
-//	private static final String RCBOSS_BASE_URL = "http://192.168.0.86:8083/";
+//	private static final String RCBOSS_BASE_URL = "http://rc.rcplatformhk.net/";
+	private static final String RCBOSS_BASE_URL = "http://192.168.0.86:8083/";
 //	private static final String RCBOSS_BASE_URL = "http://192.168.0.114/";
 
 	private static final String BASE_URL_FOR_RCBOSS = RCBOSS_BASE_URL + "rcboss/";
